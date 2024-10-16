a9551971873c288599e186720edfca837850cae1
WZB <150590206+BJlaoma@users.noreply.github.com>
2024-10-16 16:32:48 +0800

    10.16更新

--- src/main/java/com/TripMap/service/SignupService.java
+++ src/main/java/com/TripMap/service/SignupService.java
@@ -1,8 +1,12 @@
 package com.TripMap.service;
 
 import org.springframework.stereotype.Service;
+
+import com.TripMap.pojo.User;
 //注册
 @Service
 public class SignupService {
-
+    public void Signup(String name,String password){
+        User user=new User(name, password);
+    }
 }
