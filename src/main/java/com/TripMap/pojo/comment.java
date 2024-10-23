05c44ccbb3139619c3539eefe0361b4774bbfb38
liangxin233666 <2762780432@qq.com>
2024-10-23 22:29:33 +0800

    测试一下能不能推送

---
+++ src/main/java/com/TripMap/pojo/comment.java
@@ -0,0 +1,13 @@
+package com.TripMap.pojo;
+
+public class comment {
+    private int like;
+
+    public int getLike() {
+        return like;
+    }
+
+    public void setLike(int like) {
+        this.like = like;
+    }
+}
